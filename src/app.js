c3c7751f53c82aef083a9de497c7df5050f500ac
Arthur5492 <arthurofr@gmail.com>
2024-10-14 15:29:32 -0300

    vey

--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
 var express = require('express');
 const webSocket = require('ws');
+var bodyParser = require('body-parser')
+const {MongoClient} = require('mongodb');
 
 var app = express();
+app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'));
-
 const wss = new webSocket.Server({ port:8080 });
+
+async function conecta() {
+  var client = new MongoClient('mongodb://127.0.0.1:27017');
+  await client.connect();
+  db = await client.db("DOOR-CONTROL");
+  teachers = await db.collection("teachers");
+  doors = await db.collection("doors");
+}
 
 //Legal de utilizar:
 //Assert() é uma função que verifica se uma expressão é verdadeira. Se a expressão for falsa, a função gera um erro que pode ser capturado e tratado. 
   //Ex: assert.equal(2, 1);Erro
-
-var portas = ["210A","122C","205A"];
 
 //APP
 app.get('/', function(req, res){
@@ -27,7 +35,7 @@ app.get('/lista', function(req,res)
   portas.forEach(function(porta, index){
     console.log("Porta: ${porta}");
   });
-  
+
 });
 
 app.listen(3000, (error) =>{
